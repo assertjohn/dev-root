6968a903746f9ae35e4e746d84c0f04d9ee718da
John Lutoslawski <john.lutosl@gmail.com>
2024-10-05 03:16:07 -0400

    added holopage project

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -8,6 +8,12 @@ export interface Project {
   
   export const projects: Project[] = [
     {
+      name: "holopage",
+      description: "Nocode VR website builder",
+      icon: "/images/holo-logo.png?height=60&width=60",
+      link: "https://holopage.app",
+    },
+    {
       name: "GPT Wrapped",
       description: "See your ChatGPT usage history visualized.",
       icon: "/images/gptwrapped-logo.png?height=60&width=60",
